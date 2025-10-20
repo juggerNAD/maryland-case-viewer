6ccaf5a080eda954af8d0016e38528f1103f77ae
juggerNAD <homeph.barisowfh@gmail.com>
2025-10-21 02:17:03 +0800

    Fix Google Sheet Issue

--- case_viewer_maryland.py
+++ case_viewer_maryland.py
@@ -23,7 +23,9 @@ def download_sheet_csv(sheet_id, sheet_name=SHEET_NAME):
             # Make a deep copy so we do NOT modify st.secrets directly
             creds_dict = copy.deepcopy(st.secrets["gcp_service_account"])
             # Convert literal \n to actual newlines
+            creds_dict = st.secrets["gcp_service_account"]
             creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
+
             creds = service_account.Credentials.from_service_account_info(creds_dict, scopes=SCOPE)
             client = gspread.authorize(creds)
         else:
